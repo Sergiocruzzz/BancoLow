d2f65f6145974455e2fb6d8b844401d31e60a239
unknown <scruz5114@gmail.com>
2022-08-08 16:19:43 -0300

    atualizando o projeto

---
+++ NewUserdin.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bancoLow
+{
+    public class NewUserdin
+    {
+        public int dinheiro;
+        
+       public NewUserdin (){
+        dinheiro = 1000;
+    }
+}
+}
